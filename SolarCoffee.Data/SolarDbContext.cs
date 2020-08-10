d7c358e3d1bcc84ee091667efde9e5e9c1802cc7
Rostyslav Palii <Rostyslav.Palii@dataart.com>
2020-08-10 11:18:42 +0300

    Add Initial migrations

---
+++ SolarCoffee.Data/SolarDbContext.cs
@@ -0,0 +1,17 @@
+﻿using SolarCoffee.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace SolarCoffee.Data
+{
+    public class SolarDbContext : IdentityDbContext
+    {
+        public SolarDbContext() {}
+
+        public SolarDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions) {}
+        
+        public virtual DbSet<Customer> Customers { get; set; }
+
+        public virtual DbSet<CustomerAddress> CustomerAddresses { get; set; }
+    }
+}
